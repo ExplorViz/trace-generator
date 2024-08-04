8f7960b674b0bf2a00d35cb82b993e9f413b96a2
Moritz Bugla <stu237520@mail.uni-kiel.de>
2024-08-04 17:09:09 +0200

    Add helper function to stringify app tree

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { FakeApp, FakePackage } from "./structure";
+
 /**
  * Takes any string and converts the first character to uppercase
  * @param text Any string of which the first character should be capitalized
@@ -33,4 +35,64 @@ export function sanitizeJavaIdentifier(identifier: string) {
   identifier = identifier.replace(/^[0-9]+/, ''); // Trim any numbers at beginning of identifier
   const isValidIdentifier: boolean = !JAVA_RESERVED_TOKENS.includes(identifier) && identifier != '';
   return isValidIdentifier ? identifier : IDENTIFIER_FALLBACK;
+}
+
+/**
+ * Turn application tree structure into a string representation.
+ * @param app Application structure to stringify
+ * @example
+ *     // Output example
+ *     org
+ *     └─tracegen
+ *       └─randomapp
+ *         ├─monitor
+ *         | ├─feed
+ *         | | ├─port
+ *         | | └─panel
+ *         | ├─card
+ *         | └─matrix
+ *         ├─transmitter
+ *         | └─array
+ *         ├─protocol
+ *         └─driver
+ */
+export function appTreeToString(app: FakeApp): string {
+  function packageTreeToString(pkg: FakePackage): string {
+    let result: string = pkg.name + '\n';
+
+    // Recursively turn subpackages to string
+
+    let subpackageStrs: Array<string> = pkg.subpackages.map(packageTreeToString);
+    subpackageStrs = subpackageStrs.map((str, idx) => {
+      // Include '|' if there are more subpackages to come after this one
+      const strWithPipe = '├─' + str.replace(/\n/g, '\n| ');
+      const strWithoutPipe = '├─' + str.replace(/\n/g, '\n  ');
+      return (idx !== subpackageStrs.length - 1 || pkg.classes.length > 0) ? strWithPipe : strWithoutPipe;
+    });
+
+    // Turn classes to string
+
+    let classStrs: Array<string> = pkg.classes.map(clazz => clazz.identifier);
+    classStrs = classStrs.map(str => '├─\x1b[33m' + str + '\x1b[0m');
+
+    // Replace '├─' with '└─' for last package/class
+
+    if (classStrs.length === 0) {
+      if (subpackageStrs.length > 0) {
+        const lastIdx = subpackageStrs.length - 1;
+          const updatedSubpackageStr = subpackageStrs[lastIdx].replace(/├─/, '└─');
+          subpackageStrs[lastIdx] = updatedSubpackageStr;
+      }
+    } else {
+      classStrs[classStrs.length - 1] = classStrs[classStrs.length - 1].replace(/├─/g, '└─');
+    }
+
+    // Reduce to final string
+
+    result += subpackageStrs.reduce((acc, str) => acc += str + '\n', '');
+    result += classStrs.reduce((acc, str) => acc += str + '\n', '');
+    return result.slice(0, -1); // Remove newline
+  }
+
+  return packageTreeToString(app.rootPackage);
 }
